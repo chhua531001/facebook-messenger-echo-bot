db812ae8b759c57123696b471efc098f63659b91
chhua531001 <chhua531001@gmail.com>
2017-10-21 17:21:58 +0800

    Update token

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let bodyParser = require('body-parser')
 let request = require('request')
 let app = express()
 
-const FACEBOOK_ACCESS_TOKEN = 'Your_Facebook_Access_Token'
+const FACEBOOK_ACCESS_TOKEN = EAAVPfMxlHoQBAHMpXFSVugMFRVoZAT7916ny8K0t8qkD5ZA82BgmCPwsUtu5Ob1H4lDtOsIteapapcqfrmS0GbRbzBc
 const PORT = process.env.PORT || 3000
-const VERIFY_TOKEN = 'Your_Verify_Token'
+const VERIFY_TOKEN = 'chatbot-class-1021'
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
