247bda41b4c528397f0cd7d19f8d97b525d54092
chhua531001 <chhua531001@gmail.com>
2017-10-21 20:57:33 +0800

    更新Token

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let bodyParser = require('body-parser')
 let request = require('request')
 let app = express()
 
-const FACEBOOK_ACCESS_TOKEN = 'EAAVPfMxlHoQBAHMpXFSVugMFRVoZAT7916ny8K0t8qkD5ZA82BgmCPwsUtu5Ob1H4lDtOsIteapapcqfrmS0GbRbzBc'
+const FACEBOOK_ACCESS_TOKEN = 'EAACfsU8sOJwBANnaYtp4jmDxplOuhWie4tPLN23La4SF9tS1NANxgX8zZCnLgJM9YB3vHFlbu6FncHzYEZB6bOiiuszZCmZA'
 const PORT = process.env.PORT || 3000
 const VERIFY_TOKEN = 'chatbot-class-1021'
 
